84d038d79cf095c5d21139c5b8ad3199a9befd6c
mangming2 <leejiho35@gmail.com>
2023-04-10 17:46:38 +0900

    update styled component

--- frontend/src/components/trade/TradeBox.tsx
+++ frontend/src/components/trade/TradeBox.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getPairContract } from "../../contracts/getPairContract";
 import { UniswapV2Pair } from "../../typechain/UniswapV2Pair";
 import { makeEtherFromBigNumber } from "../../utils/transform";
+import styled from "styled-components";
 
 function TradeBox() {
   const [pairContract, setPairContract] = useState<UniswapV2Pair>();
@@ -17,15 +18,87 @@ function TradeBox() {
   };
 
   useEffect(() => {
-    getContract();
+    //getContract();
   }, []);
 
   return (
-    <div>
-      <h1>Trad1e</h1>
+    <Wrap>
+      <div>SWAP</div>
+      <div className="flex justify-center items-center w-88 h-48 bg-[#0D0D2B]">
+        <div>
+          <div className="flex">
+            <input type="text" />
+            <select>
+              <option value="ETH">ETH</option>
+              <option value="USDT">USDT</option>
+            </select>
+          </div>
+          <div className="flex">
+            <input type="text" />
+            <select>
+              <option value="ETH">ETH</option>
+              <option value="USDT">USDT</option>
+            </select>
+          </div>
+        </div>
+        <div>
+          <button>Swap</button>
+        </div>
+      </div>
       <button onClick={getPairBalance}>Get Pair Balance</button>
-    </div>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #0d0d2b;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  .flex {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  input {
+    width: 200px;
+    height: 40px;
+    background-color: #0d0d2b;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 700;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+  }
+  select {
+    width: 100px;
+    height: 40px;
+    background-color: #0d0d2b;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 700;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+  }
+  button {
+    width: 200px;
+    height: 40px;
+    background-color: #0d0d2b;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 700;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+  }
+`;
 
 export default TradeBox;
